5ea879fb275a9dfd2d1fb043948c3631dcfd4b49
vladisluw <komkov222111@gmail.com>
2023-10-21 13:33:32 +0500

    added
    - header

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -20,7 +20,7 @@ export const path ={
     scss: `${srcFolder}/scss/style.scss`,
     html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`,
-    svgicons: `${srcFolder}/svgicons/*.svg`,
+    svgicons: `${srcFolder}/icons/*.svg`,
   },
   watch:{
     js: `${srcFolder}/js/**/*.js`,
